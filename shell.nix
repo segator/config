f3ae96690f3c5c9a5c2982e075d04d6f79fca6ef
Isaac Aymerich Franco <isaac.aymerich@roche.com>
2024-02-11 23:21:29 +0100

    fix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,11 @@
-{ pkgs, ...}:
-    with pkgs;
-        mkShell {
-        buildInputs = [
+{ pkgs, ...}:{    
+    default =  pkgs.mkShell {
+        buildInputs = with pkgs; [
             git just age ssh-to-age sops moreutils
         ];
 
         shellHook = ''
             export SOPS_AGE_KEY_FILE=~/.secrets/nix/age_user_key.txt
         '';
+    };
 }
