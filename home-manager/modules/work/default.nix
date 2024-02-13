ac262c151e36d7c811c5c9add471b5c961c33662
Isaac Aymerich <isaac.aymerich@roche.com>
2024-02-13 22:57:25 +0100

    slack screen sharing

--- home-manager/modules/work/default.nix
+++ home-manager/modules/work/default.nix
@@ -1,6 +1,22 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+let
+  # This overlay is needed to allow screen sharing in wayland
+  slack = pkgs.slack.overrideAttrs (oldAttrs: rec {
+
+
+    fixupPhase = ''
+      sed -i -e 's/,"WebRTCPipeWireCapturer"/,"LebRTCPipeWireCapturer"/' $out/lib/slack/resources/app.asar
+
+      rm $out/bin/slack
+      makeWrapper $out/lib/slack/slack $out/bin/slack \
+        --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
+        --suffix PATH : ${lib.makeBinPath [ pkgs.xdg-utils ]} \
+        --add-flags "--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations,WebRTCPipeWireCapturer"
+    ''; 
+  });
+in
 {
-  home.packages = with pkgs; [
+  home.packages = [
     slack
   ];
 }
