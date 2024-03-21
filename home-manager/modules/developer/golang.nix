240424430497d280c31ca2eac900ab8a165a3008
Isaac Aymerich <isaac.aymerich@roche.com>
2024-03-21 09:33:23 +0100

    gh copilot

--- home-manager/modules/developer/golang.nix
+++ home-manager/modules/developer/golang.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   home.packages = with pkgs; [
-    jetbrains.goland
+    (jetbrains.plugins.addPlugins jetbrains.goland [ "github-copilot" ])
     go_1_20
     gcc  
   ];
--- home-manager/modules/developer/java.nix
+++ home-manager/modules/developer/java.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 {
   home.packages = with pkgs; [
-    jetbrains.idea-ultimate
+    (jetbrains.plugins.addPlugins jetbrains.idea-ultimate [ "github-copilot" ])
   ];
 }
