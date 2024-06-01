4e66dce6ea9db95fa79b8b4a0a9ac1a130b03f06
Isaac Aymerich <isaac.aymerich@gmail.com>
2024-06-01 14:07:47 +0200

    remove some hardcoded paths

--- nixos/host/seganas/persistence.nix
+++ nixos/host/seganas/persistence.nix
@@ -23,13 +23,15 @@
     directories = 
     (lib.optionals ((lib.attrNames config.security.acme.certs)!=[]) ["/var/lib/acme"])
     ++ 
-    (lib.optionals config.services.nextcloud.enable ["/var/lib/nextcloud"])
+    (lib.optionals config.services.nextcloud.enable [config.services.nextcloud.datadir])
     ++
-    (lib.optionals config.services.postgresql.enable ["/var/lib/postgresql"])
+    (lib.optionals config.services.postgresql.enable [config.services.postgresql.dataDir])
+    ++
+    (lib.optionals config.services.postgresqlBackup.enable [config.services.postgresqlBackup.location])
     ++
     (lib.optionals config.services.samba.enable ["/var/lib/samba"])
     ++
-    (lib.optionals config.services.rabbitmq.enable ["/var/lib/rabbitmq"])
+    (lib.optionals config.services.rabbitmq.enable [config.services.rabbitmq.dataDir])
     ++
     (lib.optionals config.services.onlyoffice.enable ["/var/lib/onlyoffice" ])
     ++
