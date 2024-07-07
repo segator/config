f61904493a7a0b9069d5550eb384dc3785ac91a4
Isaac Aymerich <isaac.aymerich@gmail.com>
2024-07-08 00:26:36 +0200

    remove ugly agent_hostname label

--- nixos/modules/grafana-agent/default.nix
+++ nixos/modules/grafana-agent/default.nix
@@ -81,101 +81,130 @@ in
           metrics_remote_write_password = config.sops.secrets."grafana/${monitoringProvider}/metrics/password".path;
         };
 
-        services.grafana-agent.settings = {
-          metrics= {
-            global = {
-              remote_write = [{
-                url = "\${METRICS_REMOTE_WRITE_URL}";
-                basic_auth.username = "\${METRICS_REMOTE_WRITE_USERNAME}";
-                basic_auth.password_file = "\${CREDENTIALS_DIRECTORY}/metrics_remote_write_password";
-              }];
-
-              scrape_interval = "60s";
-            };
-            configs = [{
-              name = "prometheus_scrape_configs";
-              scrape_configs = (map (promJob: {
-                job_name = promJob.name;
-                inherit (promJob) scheme;
-                static_configs = [{
-                  targets = [promJob.target];
-                  labels = {
-                    job = promJob.name;
-                    host = config.networking.hostName;
-                  };
-                }];
-              }) config.my.monitoring.prom-exporters); 
-            }];
-          };
-          logs.configs = [
-            {
-            name = "default";
-            scrape_configs = 
-              (
-                map (logJob: 
-                  {
-                    job_name = logJob.name;
-                    static_configs = [{
-                      targets = [ "localhost" ];
-                      labels = {
-                        job = logJob.name;
-                        host = config.networking.hostName;
-                        "__path__" = logJob.path;
-                      };
-                    }];                      
-                  }
-                )
-              config.my.monitoring.logs
-              )
-              ++
-              [              
+        services.grafana-agent = {
+          enable = true;
+          settings = 
+          let
+            relabelConfig-agent_hostname-instance = [
               {
-                job_name = "systemd-journal";
-                journal = {
-                  max_age = "12h";
-                  labels.job = "systemd-journal";
-                };
-                relabel_configs = [            
-                  {
-                    source_labels = [ "__journal__systemd_unit" ];
-                    target_label = "systemd_unit";
-                  }
-                  {
-                    source_labels = [ "__journal__hostname" ];
-                    target_label = "hostname";
-                  }
-                  {
-                    source_labels = [ "__journal_syslog_identifier" ];
-                    target_label = "syslog_identifier";
-                  }
-                  {
-                    source_labels = [ "__journal__pid" ];
-                    target_label = "pid";
-                  }
-                  {
-                    source_labels = [ "__journal__uid" ];
-                    target_label = "uid";
-                  }
-                  {
-                    source_labels = [ "__journal__transport" ];
-                    target_label = "transport";
-                  }
-                ];
+                action = "replace";
+                source_labels = ["agent_hostname"];
+                target_label = "instance";
+              }
+              {
+                action = "labeldrop";
+                regex = "^agent_hostname$";
               }
             ];
+          in
+          {
+            integrations = {
+              agent = {
+                enabled = true;
+                relabel_configs = relabelConfig-agent_hostname-instance;
+                metric_relabel_configs = relabelConfig-agent_hostname-instance;
+              };
+              node_exporter = {
+                enabled = true;
+                relabel_configs = relabelConfig-agent_hostname-instance;
+                metric_relabel_configs = relabelConfig-agent_hostname-instance;
+              };
+            };
+            metrics= {
+              global = {
+                remote_write = [{
+                  url = "\${METRICS_REMOTE_WRITE_URL}";
+                  basic_auth.username = "\${METRICS_REMOTE_WRITE_USERNAME}";
+                  basic_auth.password_file = "\${CREDENTIALS_DIRECTORY}/metrics_remote_write_password";
+                }];
 
-            positions.filename = "\${STATE_DIRECTORY}/loki_positions.yaml";
-            clients = [{
-              url = "\${LOGS_REMOTE_WRITE_URL}";
-              basic_auth.username = "\${LOGS_REMOTE_WRITE_USERNAME}";
-              basic_auth.password_file = "\${CREDENTIALS_DIRECTORY}/logs_remote_write_password";
+                scrape_interval = "60s";
+              };
+              configs = [{
+                name = "prometheus_scrape_configs";
+                scrape_configs = (map (promJob: {
+                  job_name = promJob.name;
+                  inherit (promJob) scheme;
+                  static_configs = [{
+                    targets = [promJob.target];
+                    labels = {
+                      job = promJob.name;
+                      host = config.networking.hostName;
+                    };
+                  }];
+                }) config.my.monitoring.prom-exporters); 
+              }];
+            };
+            logs.configs = [
+              {
+              name = "default";
+              scrape_configs = 
+                (
+                  map (logJob: 
+                    {
+                      job_name = logJob.name;
+                      static_configs = [{
+                        targets = [ "localhost" ];
+                        labels = {
+                          job = logJob.name;
+                          host = config.networking.hostName;
+                          "__path__" = logJob.path;
+                        };
+                      }];                      
+                    }
+                  )
+                config.my.monitoring.logs
+                )
+                ++
+                [              
+                {
+                  job_name = "systemd-journal";
+                  journal = {
+                    max_age = "12h";
+                    labels.job = "systemd-journal";
+                  };
+                  relabel_configs = [            
+                    {
+                      source_labels = [ "__journal__systemd_unit" ];
+                      target_label = "systemd_unit";
+                    }
+                    {
+                      source_labels = [ "__journal__hostname" ];
+                      target_label = "hostname";
+                    }
+                    {
+                      source_labels = [ "__journal_syslog_identifier" ];
+                      target_label = "syslog_identifier";
+                    }
+                    {
+                      source_labels = [ "__journal__pid" ];
+                      target_label = "pid";
+                    }
+                    {
+                      source_labels = [ "__journal__uid" ];
+                      target_label = "uid";
+                    }
+                    {
+                      source_labels = [ "__journal__transport" ];
+                      target_label = "transport";
+                    }
+                  ];
+                }
+              ];
+
+              positions.filename = "\${STATE_DIRECTORY}/loki_positions.yaml";
+              clients = [{
+                url = "\${LOGS_REMOTE_WRITE_URL}";
+                basic_auth.username = "\${LOGS_REMOTE_WRITE_USERNAME}";
+                basic_auth.password_file = "\${CREDENTIALS_DIRECTORY}/logs_remote_write_password";
+              }];
             }];
-          }];
-          
+            
 
+          };
         };
 
-        services.grafana-agent.enable = true;
+
         systemd.services.grafana-agent = {
           serviceConfig = {
             DynamicUser = lib.mkForce false;
