5bc784db753e4e813f2fbb2419550825e7c82ce7
Isaac Aymerich <isaac.aymerich@roche.com>
2025-05-30 15:41:52 +0200

    install task go

--- home-manager/modules/devops/default.nix
+++ home-manager/modules/devops/default.nix
@@ -11,6 +11,7 @@ in
   ];
   home.packages = with pkgs; [
       devbox
+      go-task
       gnumake
       gh
       lazydocker
--- home-manager/modules/shell/lsd.nix
+++ home-manager/modules/shell/lsd.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }:
 {
   programs.lsd.enable = true;
-  programs.lsd.enableAliases= true;
+  programs.lsd.enableBashIntegration = true;
+  #programs.lsd.enableAliases= true;
 }
